e4636e0af098391ac0e427016c42b99560a247aa
Ronvic2303 <58205103+ronvic12@users.noreply.github.com>
2023-06-02 20:21:15 -0700

    Created Helicopter folder

---
+++ Helicopter/Helicopter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Helicopter
+//
+//  Created by Ronvic Ernest Cuevas on 6/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Helicopter/Helicopter/HelicopterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelicopterApp.swift
+//  Helicopter
+//
+//  Created by Ronvic Ernest Cuevas on 6/2/23.
+//
+
+import SwiftUI
+
+@main
+struct HelicopterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
